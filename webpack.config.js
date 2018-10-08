b42c995def76c85fed8376e4a5674501fc3a8514
Brandon Stull <bstull@ucf.edu>
2018-10-08 16:18:48 -0400

    Cleanup and corrections to package.json. Cleanup to html files. Removed gulp build process, transitioned to webpack build process with MWDK. Removed unnecessary files.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+let srcPath = path.join(process.cwd(), 'src')
+let outputPath = path.join(process.cwd(), 'build')
+
+// load the reusable legacy webpack config from materia-widget-dev
+let webpackConfig = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig({
+	preCopy: [
+		{
+			from: srcPath+'/player.js',
+			to: outputPath
+		}
+	]
+})
+
+delete webpackConfig.entry['player.js']
+webpackConfig.entry['player.css'] = [
+	path.join(__dirname, 'src', 'player.html'),
+	path.join(__dirname, 'src', 'player.css')
+]
+
+module.exports = webpackConfig
